6273676476bb0c9a5cd4699b98f933daa8b83a2b
Vinhanova <tiagovinhanova@gmail.com>
2023-04-16 20:52:06 +0100

    Initial commit

---
+++ Calculator/Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Tiago Vinhanova on 16/04/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# cpp-small-projects
+ 
